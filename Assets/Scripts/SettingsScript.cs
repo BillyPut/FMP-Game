3d06676791f7052ba87b14f513e37d10a98a3e3d
BillyPut <520419@chrome.escg.ac.uk>
2022-05-09 15:56:29 +0100

    MenuStart

---
+++ Assets/Scripts/SettingsScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsScript : MonoBehaviour
+{
+   
+
+
+   public void SetQuality (int qualityIndex)
+   { 
+      
+        QualitySettings.SetQualityLevel(qualityIndex);
+       
+   }
+
+   public void SetFullscreen (bool isFullscreen)
+   {
+        Screen.fullScreen = isFullscreen;
+   }
+}
